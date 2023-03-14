6c8047379ab04b2d5c380abad9109e0a76a02938
Taylor Ambrose <taykesby@gmail.com>
2023-03-14 11:39:24 -0400

    lab complete and reviewed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,32 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for index, name in enumerate(names):
+        print(f"{index + 1}. {name}")
+    # or
+    # count = 1
+    # for name in names:
+    #     print(f"{count}. {name}")
+    #     count += 1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    new_list = []
+    for call in calls:
+        new_list.append(f"{call.capitalize()}!")
+    return new_list
+    # or
+    # return [call.capitalize() + "!" for call in calls]
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(words_list):
+    for word in words_list:
+        if len(word) > 4:
+            return True
+    return False
+
+
+def find_the_cheese(snack_list):
+    the_cheeses = ["cheddar", "gouda", "camembert"]
+    for the_snack in snack_list:
+        if the_snack in the_cheeses:
+            return the_snack
+    return None
